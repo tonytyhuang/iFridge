319b06e948939e6de0eefa10dc25c7fbda08ca80
YiJie (Jackie) Zhu <64090163+jckpokimain@users.noreply.github.com>
2020-08-22 01:09:17 -0400

    tony butt flat

---
+++ lib/screens/authenticate/sign_in.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:fridge_app/services/auth.dart';
+
+class SignIn extends StatefulWidget {
+  @override
+  _SignInState createState() => _SignInState();
+}
+
+class _SignInState extends State<SignIn> {
+  final AuthService _auth = AuthService();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.brown[100],
+      appBar: AppBar(
+          backgroundColor: Colors.brown[400],
+          elevation: 0.0,
+          title: Text('Sign In')),
+      body: Container(
+        padding: EdgeInsets.symmetric(vertical: 20.0, horizontal: 50),
+        child: RaisedButton(
+            child: Text('Sign in anon'),
+            onPressed: () async {
+              dynamic result = await _auth.signInanon();
+            }),
+      ),
+    );
+  }
+}
