e72e42ba0b2f62e74ce63b12b598569372ba2a9e
YiJie (Jackie) Zhu <64090163+jckpokimain@users.noreply.github.com>
2020-08-22 12:48:36 -0400

    tony peepee miniscule

--- lib/services/auth.dart
+++ lib/services/auth.dart
@@ -34,4 +34,28 @@ class AuthService {
       return null;
     }
   }
+
+  Future registerWithEmailAndPassword(String email, String password) async {
+    try {
+      AuthResult result = await _auth.createUserWithEmailAndPassword(
+          email: email, password: password);
+      FirebaseUser user = result.user;
+      return _userFromFirebaseUser(user);
+    } catch (e) {
+      print(e.toString());
+      return null;
+    }
+  }
+
+  Future signInWithEmailAndPassword(String email, String password) async {
+    try {
+      AuthResult result = await _auth.signInWithEmailAndPassword(
+          email: email, password: password);
+      FirebaseUser user = result.user;
+      return _userFromFirebaseUser(user);
+    } catch (e) {
+      print(e.toString());
+      return null;
+    }
+  }
 }
