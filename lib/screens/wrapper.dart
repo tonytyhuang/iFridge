6986c59e6e8c818e0b9e48f4442298af259e77b7
YiJie (Jackie) Zhu <64090163+jckpokimain@users.noreply.github.com>
2020-08-22 00:17:18 -0400

    add new files

---
+++ lib/screens/wrapper.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import 'package:fridge_app/screens/home/home.dart';
+
+class Wrapper extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Home();
+  }
+}
